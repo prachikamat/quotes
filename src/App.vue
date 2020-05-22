fe4c0cd4b196db6dd4224bcd62b76bc4c1369890
prachikamat <prachi.kamat96@yahoo.com>
2020-05-22 14:01:17 +0530

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,61 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!-- Header -->
+    <app-header :quoteCount="quotes.length" :maxQuotes="maxQuotes"></app-header>
+    <!-- New Quote  -->
+    <app-new-quote @quoteAdded="newQuote"></app-new-quote>
+   <!-- Quote Grid -->
+    <app-quote-grid :quotes="quotes"
+    @quoteDeleted="deleteQuote"></app-quote-grid>
+    <!-- Notice -->
+    <div class="row">
+      <div class="col-sm-12 text-center">
+        <div class="alert alert-success">
+          Notice: Click on a Quote to delete it!
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import QuoteGrid from './components/QuoteGrid.vue';
+import NewQuote from './components/NewQuote.vue';
+import Header from './components/Header.vue';
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  export default {
+    data: function() {
+      return {
+        quotes: [
+          'Love all, trust a few, do wrong to none'
+        ],
+        maxQuotes: 12
+      };
+    },
+    methods: {
+        newQuote(quote) {
+          if(this.quotes.length >= this.maxQuotes) {
+              return alert('Please delete quotes first!');
+          }
+            this.quotes.push(quote);
+        },
+        deleteQuote(index) {
+          this.quotes.splice(index,1);
+        }
+    },
+    components: {
+      appQuoteGrid: QuoteGrid,
+      appNewQuote: NewQuote,
+      appHeader: Header
+    }
   }
-}
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+.row {
+  margin: 0;
+  padding: 0;
+  margin-right: 0 !important;
+  margin-left: 0 !important;
 }
 </style>
---
+++ src/components/Quote.vue
@@ -0,0 +1,28 @@
+<template>
+ <div class="com-sm-6 col-md-4 col-lg-3">
+      <div class="panel panel-default">
+        <div class="panel-body quote">
+          <slot></slot>
+        </div>
+      </div>
+  </div>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style>
+
+  .panel-body {
+        font-family: 'Arizonia', cursive;
+        font-size: 24px;
+        color-adjust: #6e6e6e;
+  }
+
+  .quote:hover {
+        background-color: #e2ccdb8c;
+  }
+
+</style>
